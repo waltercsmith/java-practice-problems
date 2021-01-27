5ef17f91f9a4f7c9c520f651a941478026ad5c60
Walter Smith <walter.c.smith01@gmail.com>
2021-01-27 17:40:31 -0600

    Completed one practice problem

--- java-practice/src/warmup.java
+++ java-practice/src/warmup.java
@@ -2,7 +2,7 @@
 public class warmup {
     /*The parameter weekday is true if it is a weekday,
         and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on                    vacation. Return true if we sleep in.*/
-    public boolean sleepIn(boolean weekday, boolean vacation) {
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
         if(weekday && vacation){
             return true;
         } if(weekday){
@@ -11,8 +11,9 @@ public class warmup {
         return true;
     }
 
-	public static void main(String[] args){
 
+	public static void main(String[] args){
+        System.out.println(sleepIn(true,false));
 
 
 	}
