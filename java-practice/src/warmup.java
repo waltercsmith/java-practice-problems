221694a6738d5f65df793794d04c37f95670f7f4
Walter Smith <walter.c.smith01@gmail.com>
2021-01-27 17:46:33 -0600

    Completed a 2nd boolean practice problem

--- java-practice/src/warmup.java
+++ java-practice/src/warmup.java
@@ -11,11 +11,21 @@ public class warmup {
         return true;
     }
 
+        /*We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. We are in    trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.*/
+    public static boolean monkeyTrouble(boolean aSmile, boolean bSmile){
+        if(aSmile == bSmile){
+            return true;
+        }
+        return false;
+    }
+
 
 	public static void main(String[] args){
         System.out.println(sleepIn(true,false));
+        System.out.println(monkeyTrouble(true, true));
+        System.out.println(monkeyTrouble(false, false));
+        System.out.println(monkeyTrouble(true, false));
 
-
-	}
+    }
 
 }
