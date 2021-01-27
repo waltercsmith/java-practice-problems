a5d3232a1b5bc0b281d56bd5ad548e9ba60cf7d2
Walter Smith <walter.c.smith01@gmail.com>
2021-01-27 17:53:15 -0600

    Completed a 3 practice problem which involve adding the sum of two parameters but if they are equal then double the sum

--- java-practice/src/warmup.java
+++ java-practice/src/warmup.java
@@ -19,12 +19,24 @@ public class warmup {
         return false;
     }
 
+    /*Given two int values, return their sum. Unless the two values are the same, then return double their sum*/
+
+    public static int sumDouble(int a, int b){
+        if(a == b){
+            return (a + b) * 2;
+        }
+            return a + b;
+    }
+
 
 	public static void main(String[] args){
         System.out.println(sleepIn(true,false));
         System.out.println(monkeyTrouble(true, true));
         System.out.println(monkeyTrouble(false, false));
         System.out.println(monkeyTrouble(true, false));
+        System.out.println(sumDouble(1, 2));
+        System.out.println(sumDouble(3, 2));
+        System.out.println(sumDouble(2, 2));
 
     }
 
