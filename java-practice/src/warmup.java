42b514d65ed99e053b40fa4e09cd34b5b8b22368
Walter Smith <walter.c.smith01@gmail.com>
2021-01-29 19:26:12 -0600

    Created a method that takes an integer and return the absolute difference between between the integer and 21, excepte return double the absolute difference if integer is over 21

--- java-practice/src/warmup.java
+++ java-practice/src/warmup.java
@@ -31,7 +31,10 @@ public class warmup {
     /*Given an int n, return the absolute difference between n and 21, except return double the absolute difference if      n is over 21*/
 
     public int diff21(int n){
-        return n -21;
+        if(n > 21){
+            return (21 -n) * -2;
+        }
+        return 21 -n;
     }
 
 
