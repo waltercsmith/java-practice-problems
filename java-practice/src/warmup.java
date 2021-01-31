6867f916519f8f8d913e4325f7bbcefead0dde29
Walter Smith <walter.c.smith01@gmail.com>
2021-01-30 18:47:24 -0600

    Added a java method that takes two arguments, one is a boolean and another an integer.

--- java-practice/src/warmup.java
+++ java-practice/src/warmup.java
@@ -37,6 +37,10 @@ public class warmup {
         return 21 -n;
     }
 
+    /*We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is       before 7 or after 20. Return true if we are in trouble*/
+
+
+
 
 	public static void main(String[] args){
         System.out.println(sleepIn(true,false));
