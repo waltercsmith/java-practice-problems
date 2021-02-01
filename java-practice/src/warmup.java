0ebb1ca7c484f450ec7e8f125f27d5fca6d8e2f0
Walter Smith <walter.c.smith01@gmail.com>
2021-01-31 19:58:16 -0600

    A method that given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.

--- java-practice/src/warmup.java
+++ java-practice/src/warmup.java
@@ -52,6 +52,8 @@ public class warmup {
     public boolean makes10(int a, int b){
         if (a == 10 || b == 10){
             return true;
+        }else if( a + b == 10){
+            return true;
         }
             return false;
     }
