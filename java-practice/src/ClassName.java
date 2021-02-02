ead334f0d200d124b374e2425fbff68517a55106
Walter Smith <walter.c.smith01@gmail.com>
2021-02-02 17:22:25 -0600

    main method within the class ClassName has two system out print strings on separate lines

--- java-practice/src/ClassName.java
+++ java-practice/src/ClassName.java
@@ -1,5 +1,7 @@
 public class ClassName {
     public static void main(String [] args){
-        System.out.print("Hello, Walter! Stay Sharp!");
+        System.out.println("Hello, Walter! Stay Sharp!");
+
+        System.out.println("Walter is awesome!");
     }
 }
