589e5bb43bf4817aedd0520749447d4300f4d465
Walter Smith <walter.c.smith01@gmail.com>
2021-02-04 11:39:24 -0600

    Created a double variable and wrote code to output said variable

--- java-practice/src/ConsoleExercises.java
+++ java-practice/src/ConsoleExercises.java
@@ -2,5 +2,15 @@ public class ConsoleExercises {
 
     public static void main(String[] args){
 
+//        Copy this code into your main method:
+    double pi = 3.14159;
+//        Write some Java code that uses the variable pi to output the following:
+//        The value of pi is approximately 3.14.
+
+
+
+        System.out.format("The value of pi is approximately " + pi);
+
+
     }
 }
