95ba9f336223bf9f61e728c2889a519b30920d81
Walter Smith <walter.c.smith01@gmail.com>
2021-02-06 19:58:12 -0600

    Created an integer variable named i with a value of 5

--- java-practice/src/ControlFlowExercises.java
+++ java-practice/src/ControlFlowExercises.java
@@ -2,6 +2,8 @@ public class ControlFlowExercises {
 //    Created a class called ControlFlowExercises w/ a main method
 
     public static void main(String[] args){
+//        Create a while loop with a integer variable with a value of 5, loop runs runs as long as i is less than 5
+    int numberFive = 5;
 
     }
 }
