61dfa40a5d428ac428976485c2411a5ba856322f
Walter Smith <walter.c.smith01@gmail.com>
2021-02-06 20:03:37 -0600

    Created a while loop which prints numbers 5 through 15 in the console on the same line with a space between each number

--- java-practice/src/ControlFlowExercises.java
+++ java-practice/src/ControlFlowExercises.java
@@ -2,8 +2,12 @@ public class ControlFlowExercises {
 //    Created a class called ControlFlowExercises w/ a main method
 
     public static void main(String[] args){
-//        Create a while loop with a integer variable with a value of 5, loop runs runs as long as i is less than 5
-    int numberFive = 5;
+//        Create a while loop with a integer variable with a value of 5, loop runs runs as long as i is less than 15
+    int i = 5;
 
+    while (i < 16){
+        System.out.print(" " + i);
+        i++;
+    }
     }
 }
