6256b5c0b44d9721ebe2db95c30852b1a81df0ce
Walter Smith <walter.c.smith01@gmail.com>
2021-02-08 10:10:20 -0600

    Created a do-while loop to count backwards by 5's from 100 to -10. Created two int variables where the first variable value is 20 and with a post decrement. The first variable also has a condition that is greater than -3. The second variable has a value of 5 and is multiplied by the first variable.

--- java-practice/src/ControlFlowExercises.java
+++ java-practice/src/ControlFlowExercises.java
@@ -2,6 +2,7 @@ public class ControlFlowExercises {
 //    Created a class called ControlFlowExercises w/ a main method
 
     public static void main(String[] args){
+//        ***WHILE LOOP***
 //        Create a while loop with a integer variable with a value of 5, loop runs runs as long as i is less than 15
 //    int i = 5;
 //
@@ -10,13 +11,27 @@ public class ControlFlowExercises {
 //        i++;
 //    }
 
+
+//        ***DO-WHILE LOOPS***
 //    Create a do-while loop that will count by 2's starting with 0 and ending with 100
-        int j = 2;
-        int i = 0;
-        do{
-            System.out.println(i * 2);
-            i++;
+//        int j = 2;
+//        int i = 0;
+//        do{
+//            System.out.println(i * 2);
+//            i++;
+//        }
+//        while (i < 51);
+
+//        Now create a do-while loop to count backwards by 5's from 100 to -10
+        int k = 20;
+        int l = 5;
+        do {
+            System.out.println(k * l);
+            k--;
         }
-        while (i < 51);
+        while (k > -3);
+
+
     }
+
 }
