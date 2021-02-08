78d630c55269d4cfddc793a8e089ef281b7358ee
Walter Smith <walter.c.smith01@gmail.com>
2021-02-08 11:38:38 -0600

    Used a for loop to refactor code that consoles 100 to -10

--- java-practice/src/ControlFlowExercises.java
+++ java-practice/src/ControlFlowExercises.java
@@ -1,7 +1,7 @@
 public class ControlFlowExercises {
 //    Created a class called ControlFlowExercises w/ a main method
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 //        ***WHILE LOOP***
 //        Create a while loop with a integer variable with a value of 5, loop runs runs as long as i is less than 15
 //    int i = 5;
@@ -23,15 +23,37 @@ public class ControlFlowExercises {
 //        while (i < 51);
 
 //        Now create a do-while loop to count backwards by 5's from 100 to -10
-        int k = 20;
-        int l = 5;
-        do {
-            System.out.println(k * l);
-            k--;
-        }
-        while (k > -3);
+//        int k = 20;
+//        int l = 5;
+//        do {
+//            System.out.println(k * l);
+//            k--;
+//        }
+//        while (k > -3);
 
 
+//        Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000.
+
+//        int m = 0;
+//        long n = 2;
+////        double square = Math.pow(m,2); // Did not work, console output 1.0
+//        do {
+//            System.out.println(m);
+//            m++;
+//            n *= n;
+//        } while (m < 256);
+
+//        **Having trouble with using a do while loop to complete this particular task
+
+//        Refactor the previos two exercises to use a FOR loop
+
+//        for (int o = 20; o > -3; o--){
+//            System.out.println(o * 5);
+//        }
+
+//        for (int p = 1; p < 128; p++){
+//            System.out.println(p * 2);
+//        }
     }
 
 }
