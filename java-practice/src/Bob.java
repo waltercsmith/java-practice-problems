a4b96f27a475f917478a00a39a0c17a3504d610d
Walter Smith <walter.c.smith01@gmail.com>
2021-02-08 20:11:30 -0600

    Java code that utilize String methods that allows the user to input a statment. Depending on the punctuation or lack there of will print out a response

--- java-practice/src/Bob.java
+++ java-practice/src/Bob.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
 
+//        Exercise 2 of Strings Module
+
+        /*Java code that uses the input into the command line to have a conversation with Bob.
+        * If the user inputs a question, exclamation, nothing or anything else will be presented
+        * by a varied response*/
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        System.out.println("You: " + userInput);
+        if (userInput.endsWith("?")){
+            System.out.println("Bob: " + "Sure.");
+        }else if (userInput.endsWith("!")){
+            System.out.println("Bob: " + "Whoa, chill out!");
+        }else if (userInput.endsWith(" ")){
+            System.out.println("Bob: " + "Fine. Be that way!");
+        }else {
+            System.out.println("Bob: Whatever.");
+        }
     }
 }
