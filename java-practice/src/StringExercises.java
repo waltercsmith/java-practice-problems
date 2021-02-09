ddf62cdfe0323c100319a842cec28e7d957bb5b9
Walter Smith <walter.c.smith01@gmail.com>
2021-02-08 19:55:49 -0600

    Created string variables and sout the variable names to print out the output

--- java-practice/src/StringExercises.java
+++ java-practice/src/StringExercises.java
@@ -1,5 +1,24 @@
 public class StringExercises {
     public static void main(String[] args){
 
+//        Exercise 1
+//        For each of the following output examples: create a String variable that contains the desired output and print it out to the console, you can do this with only one String variable and one print statement for each output example.
+
+        String statement1 = "We don't need no education";
+        System.out.println(statement1);
+
+        String statement2 = "We don't need no thought control";
+        System.out.println(statement2);
+
+        String statement3 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(statement3);
+
+        String statement4 = "In windows, the main drive is usually C:\\";
+        System.out.println(statement4);
+
+        String statement5 = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(statement5);
+
     }
 }
