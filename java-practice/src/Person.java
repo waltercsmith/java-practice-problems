dfb5586766f5699bf9a9c01b2530327bdd2c10ab
Walter Smith <walter.c.smith01@gmail.com>
2021-02-11 06:20:46 -0600

    Created a main method. Created a constructor that accepts a string value and sets the person's name to the passed string

--- java-practice/src/Person.java
+++ java-practice/src/Person.java
@@ -3,4 +3,31 @@ public class Person {
     //Created private String property called name
     private String name;
 
+
+    //This is a constructor that accepts a string value & sets the person's name to the passed string
+    public Person(String aPersonsName){
+         this.name = aPersonsName;
+    }
+
+    //Needs to return the person's name
+    public String getName(){
+        return this.name = name;
+    }
+
+    //Needs to change the name property to the passed value
+    public void setName(String name){
+
+    }
+
+
+    //Needs to print to the console using the person's name
+    public void sayHello(){
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
 }
