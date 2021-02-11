738d35ce33dff6e6ac31495aba09dc24eb0a097f
Walter Smith <walter.c.smith01@gmail.com>
2021-02-11 06:42:10 -0600

    Used commented out Comparison Code to observe if methods functioned properly such as the setName method changes the name property to the passed value

--- java-practice/src/Person.java
+++ java-practice/src/Person.java
@@ -15,8 +15,8 @@ public class Person {
     }
 
     //Needs to change the name property to the passed value
-    public void setName(String name){
-
+    public void setName(String newName){
+        this.name = newName;
     }
 
 
@@ -29,10 +29,27 @@ public class Person {
     stating that cannot return a value from a method with void result type*/
 
     public static void main(String[] args) {
-        Person walt = new Person("Walt");
-        walt.sayHello();
+//        Person walt = new Person("Walt");
+//        walt.sayHello();
 
+        //Beginning of Comparison Code is within these two comment lines
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
 
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+        //End of Comparison Code
     }
 
 }
