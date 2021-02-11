1fdc03f15f8ff67df34d859627d362c90c11551d
Walter Smith <walter.c.smith01@gmail.com>
2021-02-11 10:20:17 -0600

    Created a public static method that returns a boolean but takes a string argument. The method has a conditional where if the user's input is 'y' then the console will display true. Any other input will display false in the console.

--- java-practice/src/util/Input.java
+++ java-practice/src/util/Input.java
@@ -20,25 +20,27 @@ public class Input {
         return someWords;
     }
 
-    public static boolean yesNo(boolean trueOrFalse){
-        if (trueOrFalse){
-            System.out.println("yes");
-        }else {
-            System.out.println("no");
+    public static boolean yesNo(String x){
+        if (x.equals("y")){
+            return true;
         }
-        return trueOrFalse;
+        return false;
     }
 
 
     public static void main(String[] args){
 
-        System.out.println("Type some words");
+//        System.out.println("Type some words");
+        System.out.println("Type \"y\" to return the boolean \"true\" any other input will result in the boolean \"false\" ");
         Scanner scanner = new Scanner(System.in);
 
         String userInput = scanner.nextLine();
-        Input input1 = new Input(userInput);
-        System.out.println(input1.getString());
+//        Input input1 = new Input(userInput);
+//        System.out.println(input1.getString());
 
+
+
+        System.out.println(yesNo(userInput));
 
 
 
