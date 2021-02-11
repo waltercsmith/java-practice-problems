569fb702d1f8110f0b977c0985b497790d126b16
Walter Smith <walter.c.smith01@gmail.com>
2021-02-11 09:37:28 -0600

    Created a method that returns the string a user inputs by creating a new instance of the input class and accessing a method using a .

--- java-practice/src/util/Input.java
+++ java-practice/src/util/Input.java
@@ -9,22 +9,30 @@ public class Input {
 
 //    private scanner; //Was told by the exercise to create a new property of scanner but prompted with problems:
 
-    Scanner scanner = new Scanner(System.in);
-    String userInput = scanner.nextLine();
 
+     private String someWords; //private field
 
-
-    public String getString(){
-        return this.userInput;
+    public Input(String words){ //my Input constructor
+        this.someWords = words;
     }
 
-    public void printGetString(){
-        System.out.println(getString());
+    public String getString(){  //Method that gets the string of a user's input
+        return someWords;
     }
+
+
 
     public static void main(String[] args){
+
+        System.out.println("Type some words");
         Scanner scanner = new Scanner(System.in);
+
         String userInput = scanner.nextLine();
+        Input input1 = new Input(userInput);
+        System.out.println(input1.getString());
+
+
+
 
 
 
