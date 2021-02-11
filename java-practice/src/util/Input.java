d1d33433de3654915fa359680c1eea7b21a14067
Walter Smith <walter.c.smith01@gmail.com>
2021-02-11 11:30:31 -0600

    Working on a public static method that returns a integer and has two integer arguments where one argument is the minimum and the second argument is the maximum. When the user enters an integer. The method holds a conditional that either tells them they are within range or prompts them to enter another number

--- java-practice/src/util/Input.java
+++ java-practice/src/util/Input.java
@@ -27,21 +27,35 @@ public class Input {
         return false;
     }
 
+    public static int getInt(int min, int max){
+        Scanner scannerInt = new Scanner(System.in);
+        int userInputInt = scannerInt.nextInt();
+        if (userInputInt >= min && userInputInt <= max){
+            System.out.printf("Great, %s  is within range",userInputInt);;
+        }else {
+            System.out.println("Please enter a number with range");
+        }
+        return getInt(min,max);
+
+    }
 
     public static void main(String[] args){
 
 //        System.out.println("Type some words");
-        System.out.println("Type \"y\" to return the boolean \"true\" any other input will result in the boolean \"false\" ");
-        Scanner scanner = new Scanner(System.in);
-
-        String userInput = scanner.nextLine();
+//        System.out.println("Type \"y\" to return the boolean \"true\" any other input will result in the boolean \"false\" ");
+//        Scanner scanner = new Scanner(System.in);
+//
+//        String userInput = scanner.nextLine();
 //        Input input1 = new Input(userInput);
 //        System.out.println(input1.getString());
 
 
 
-        System.out.println(yesNo(userInput));
-
+//        System.out.println(yesNo(userInput));
+        System.out.println("Please enter between 1 and 5");
+        Scanner scannerInt = new Scanner(System.in);
+        int userInputInt = scannerInt.nextInt();
+        System.out.println(getInt(1,5));
 
 
 
