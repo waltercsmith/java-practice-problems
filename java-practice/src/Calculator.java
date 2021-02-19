7b9e121c7c6e91943162d1fe53e604651d518771
Walter Smith <walter.c.smith01@gmail.com>
2021-02-18 19:34:43 -0500

    Created a static void method that contains an two conditionals. If the the user inputs the string quit then the prompt ends. Otherwise the user can continue to input strings

--- java-practice/src/Calculator.java
+++ java-practice/src/Calculator.java
@@ -3,34 +3,41 @@ import java.util.Scanner;
 public class Calculator {
 
 
-    static void input(){
+    static void input() {
         System.out.println("Type something");
         Scanner sc2 = new Scanner(System.in);
         String userInput2 = sc2.nextLine();
-    }
-
-    static  void  sayHello(){
-        System.out.println("Hey, how are you?");
-    }
-
-    public static void main(String[] args) {
-
-        System.out.println("Type something");
-
-        Scanner sc = new Scanner(System.in);
-        String userInput = sc.nextLine();
-
-        System.out.println(userInput);
-        if (userInput.equals("quit")){
+        if (userInput2.equals("quit")) {
             System.out.println("Ending prompt");
-        }
-        else {
+        } else {
             input();
-//            System.out.println("Type something");
-//            Scanner sc1 = new Scanner(System.in);
-//            String userInput1 = sc.nextLine();
-//            System.out.println(userInput1);
+
         }
+    }
+
+        static void sayHello () {
+            System.out.println("Hey, how are you?");
+        }
+
+        public static void main (String[]args){
+
+            System.out.println("Type something");
+
+            Scanner sc = new Scanner(System.in);
+            String userInput = sc.nextLine();
+
+            input();
+//        System.out.println(userInput);
+//        if (userInput.equals("quit")){
+//            System.out.println("Ending prompt");
+//        }
+//        else {
+//            input();
+////            System.out.println("Type something");
+////            Scanner sc1 = new Scanner(System.in);
+////            String userInput1 = sc.nextLine();
+////            System.out.println(userInput1);
+//        }
 
 
 //        System.out.println(userInput);
@@ -38,5 +45,7 @@ public class Calculator {
 //            System.out.println("Ending prompt");
 //        }
 
+        }
+
     }
-}
+
