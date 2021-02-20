f322a47279755f9ac621d13ced0008526b9fe8d7
Walter Smith <walter.c.smith01@gmail.com>
2021-02-19 20:07:40 -0500

    Created a conditional that if the user types 1 then the user will be able to continue to type inputs and have each input displayed within the console. If the user inputs 2 then the console prints Goodbye! and ends the program

--- java-practice/src/Options.java
+++ java-practice/src/Options.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Options {
 //    Note: Goal is to prompt the user with a series of questions and depending on the answer will provide a different option
 
     public static void main(String[] args) {
+        System.out.println("Would you like to begin?\n Type \"1\" for yes\n Type \"2\" for no");
+        Scanner optionsScanner = new Scanner(System.in);
+        String userInput = optionsScanner.nextLine();
+
+
+        System.out.println(userInput);
+
+        if (userInput.equals("1")){
+             Calculator.inputTest();
+        }
+        else {
+            System.out.println("Goodbye!");
+        }
 
     }
 }
