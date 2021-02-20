891b4c0a10f8e8335f2a801fcfa30a09f777e2c6
Walter Smith <walter.c.smith01@gmail.com>
2021-02-19 19:57:10 -0500

    Created another public class to prompt the user with a series of questions based on the user's response

---
+++ java-practice/src/Options.java
@@ -0,0 +1,7 @@
+public class Options {
+//    Note: Goal is to prompt the user with a series of questions and depending on the answer will provide a different option
+
+    public static void main(String[] args) {
+
+    }
+}
