34981d1bba0fc638d70afbdee81b9f180bfb5983
Walter Smith <walter.c.smith01@gmail.com>
2021-03-08 19:12:00 -0500

    Created a new intance of the OopMain class which called the constructor

--- java-practice/src/java_2_review/OopMain.java
+++ java-practice/src/java_2_review/OopMain.java
@@ -5,6 +5,10 @@ public class OopMain {
 
      public String studentName; //  <-- this is a FIELD and a VARIABLE (property)
 
+//    * A FIELD tends to be protected or private in scope
+
+//    * A PROPERTY tends be public in scope
+
 
     public String printName(String x){return x; } // <-- this is also a FIELD but it's a METHOD (function)
 
@@ -17,12 +21,37 @@ public class OopMain {
 
 //    --------------------      -------------------------------
 
+
+//    ---- ⭐︎⭐︎ STATIC FIELDS/METHODS can be accessed without creating a new instance of a class first ⭐︎⭐︎ ----
+
+
+
     public String sayHello(){ return String.format("Welcome %s", studentName); }
 
 //    ^^ A public method that takes no arguments but returns string statement w/ a STRING property (studentName)
 
 
 
+    public static int waltsAge = 30; // <-- this is static property
+
+
+//    --- CONSTRUCTORS ---
+
+/*   A CONSTRUCTOR is a special method that is called when a class is created
+        HAS:
+            SAME NAME as the class
+            DOES NOT DEFINE A RETURN TYPE
+
+            BUT
+
+            CAN ACCEPT ARGUMENTS & BE OVERLOADED
+
+ */
+
+    public OopMain(){
+        System.out.println("I am a CONSTRUCTOR of the OopMain Class\n Whenever a class is created...\n So AM I!!!");
+
+    }
 
 
     public static void main(String[] args) {
@@ -30,12 +59,24 @@ public class OopMain {
 
         OopMain walt = new OopMain(); // <-- new instance of the OopMain class
 
+        System.out.println();
+
         walt.studentName = "Walt";
 
         System.out.println(walt.sayHello()); // <-- using a new instance of the OopMain class to access a property to print a string
 
 
+        System.out.println();
 
+        System.out.println(OopMain.waltsAge); // <--- referencing the OopMain class to access the static property of waltsAge
+
+        System.out.println();
+
+    OopMain instanceOne = new OopMain();
+
+        System.out.println();
+
+    OopMain instanceTwo = new OopMain();
 
     }
 
