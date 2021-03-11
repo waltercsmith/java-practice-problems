b6d4dee48b7ca3cbd52240952b45001412362ff9
Walter Smith <walter.c.smith01@gmail.com>
2021-03-10 19:07:18 -0500

    Created a new class called arrays. Created an variable that is an array with each element is a double.

--- java-practice/src/java_2_review/Arrays.java
+++ java-practice/src/java_2_review/Arrays.java
@@ -4,7 +4,16 @@ public class Arrays {
 
 //    Arrays are a special kind of object that contain zero or more items called elements
 
-    double[] prices; // variable named prices, type is an array which each element is a double
+   static double[] prices; // variable named prices, type is an array which each element is a double
 
+//Arrays have a fixed length when created an is accessed through: .length
+
+
+
+    public static void main(String[] args) {
+
+        prices = new double[4];  //  assigned a value to prices that is an array with 4 elements
+
+    }
 
 }
