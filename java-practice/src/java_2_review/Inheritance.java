01dc265679d6faabafc1712d95ae3bfdb143809b
Walter Smith <walter.c.smith01@gmail.com>
2021-03-13 15:06:16 -0500

    Created a subclass called Inheritance that extends the class called OopMain which is the superclass

---
+++ java-practice/src/java_2_review/Inheritance.java
@@ -0,0 +1,14 @@
+package java_2_review;
+
+public class Inheritance extends OopMain{
+
+//    CLASSES can inherit another class by using the keyword EXTENDS (in lowercase)
+
+//    SUBCLASSES are classes that inherit from another classes which would be called SUPERCLASSES
+
+//    A subclass inherits all the fields of it's superclass, and can re-use any of it's functionality.
+
+
+
+
+}
