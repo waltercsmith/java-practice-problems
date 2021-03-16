945b25c1a1edde5f83154b3f081ee7fa86d7f96f
Walter Smith <walter.c.smith01@gmail.com>
2021-03-16 16:39:32 -0400

    Created a colletion of String items  using an HashMap. Then added each element using the put method.

--- java-practice/src/java_2_review/Collections.java
+++ java-practice/src/java_2_review/Collections.java
@@ -2,6 +2,8 @@ package java_2_review;
 
 import java.util.ArrayList;
 
+import java.util.HashMap;
+
 public class Collections {
 
 
@@ -17,7 +19,25 @@ public class Collections {
         animeNames.add("Guts");
         System.out.println(animeNames);
         System.out.println();
-        System.out.println("This is the first element within the array called animeNames: \n" + animeNames.get(0));
+        System.out.println("This is the first element within the array called animeNames: \n" + animeNames.get(0) + "\n \n");
+
+//        Create a HashMap to store items
+
+        HashMap<String, String> attackNames = new HashMap<String, String>();
+
+//        Adding keys and values (Title, Attack Name)
+
+        attackNames.put("Street Fighter", "Hadoken");
+        attackNames.put("Tekken", "Dragon Uppercut");
+        attackNames.put("King of Fighters", "100 Shiki: Oniyaki");
+
+        System.out.println();
+        System.out.println(attackNames);
+        System.out.println();
+//        Accessing an item from the HashMap
+
+        System.out.println("A famous move in the videogame Street Fighter is: " + attackNames.get("Street Fighter"));
+
     }
 
 
