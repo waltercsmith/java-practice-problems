ca39fc2061483759bde61bd5d8c2af130a7d3a31
Walter Smith <walter.c.smith01@gmail.com>
2021-03-16 17:09:31 -0400

    Created a getter for the Annotation class and used a deprecated annotation on an inner class called Freeze

---
+++ java-practice/src/java_2_review/Annotations.java
@@ -0,0 +1,19 @@
+package java_2_review;
+
+public class Annotations {
+    private String name;
+
+
+    @Getter
+    public String getName() {
+        return name;
+    }
+
+
+    @Deprecated
+    public class Freeze{
+
+    }
+
+
+}
---
+++ java-practice/src/java_2_review/Getter.java
@@ -0,0 +1,4 @@
+package java_2_review;
+
+public @interface Getter {
+}
